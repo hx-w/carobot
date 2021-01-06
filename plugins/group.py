cf32345334a1534e96625d0604f947ffdf96b2b0
hx-w <17361019875@163.com>
2021-01-06 21:15:56 +0800

    更新目录

--- plugins/group.py
+++
@@ -1,7 +0,0 @@
-import nonebot
-from nonebot import on_notice, NoticeSession
-
-@on_notice('group_increase')
-async def _(session: NoticeSession):
-    await session.send('欢迎新朋友!', at_sender=True)
-
